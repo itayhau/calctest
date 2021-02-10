7f4df3a163fa1c75e23d1fa90126d5c805066c66
itayhau <itayhau@gmail.com>
2021-02-10 20:23:17 +0200

    Add files via upload

---
+++ CalcTest/CalculatorTest.cs
@@ -0,0 +1,50 @@
+using DemoTest040221;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalcTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Calculator_Add_SmallNumbers()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+
+            // Act
+            var result = calc.Add(3, 4); // come form DB
+
+            // Assert
+            Assert.AreEqual(result, 7); // come form DB
+        }
+        [TestMethod]
+        public void Calculator_Add_LargeNumbers()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+
+            // Act
+            var result = calc.Add(3, 4); // come form DB
+
+            // Assert
+            Assert.AreEqual(result, 7); // come form DB
+        }
+
+        [TestMethod]
+        public void Calculator_Add_Zeros()
+        {
+
+            // Arrange
+            Calculator calc = new Calculator();
+
+            // Act
+            var result = calc.Add(3, 0); // come form DB
+
+            // Assert
+            Assert.AreEqual(result, 3); // come form DB
+            
+        }
+    }
+}
